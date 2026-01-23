c13c6020cd057561bc4a940e2a7bf41bdf411f8f
Marcio <mrparma5d@gmail.com>
2026-01-22 21:22:35 -0300

    adicionados metodos get, delete e login

---
+++ src/main/java/com/marcio/usuario/infrastructure/exception/ConflictException.java
@@ -0,0 +1,12 @@
+package com.marcio.usuario.infrastructure.exception;
+
+public class ConflictException extends RuntimeException{
+
+    public ConflictException(String mensagem){
+        super(mensagem);
+    }
+
+    public ConflictException(String mensagem, Throwable throwable){
+        super(mensagem);
+    }
+}
--- src/main/java/com/marcio/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/marcio/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,6 @@
 package com.marcio.usuario.infrastructure.repository;
 
-import com.marcio.programandospring.infrastructure.entity.Telefone;
+import com.marcio.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
